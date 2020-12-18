ad45f07d89eaea6eee69038a848f02222164bb2c
yangshun1996 <544801778@qq.com>
2020-12-18 16:01:53 +0800

    sdk

--- JAVA demo20201012/demo/src/main/java/com/fh/App.java
+++ JAVA demo20201012/demo/src/main/java/com/fh/App.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
- * Hello world!
+ * Hello world!111
  *
  */
 //@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
